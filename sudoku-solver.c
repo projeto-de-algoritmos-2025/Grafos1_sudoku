b111c1f85e2a9dba2131520f18040dad02fd31c3
DANIEL NUNES DUARTE <101228207+DanNunes777@users.noreply.github.com>
2025-04-18 20:53:46 -0300

    Update sudoku-solver.c
    Finished

--- sudoku-solver.c
+++ sudoku-solver.c
@@ -74,3 +74,27 @@ bool resolverSudoku(int tabela[N][N]) {
     return false;
 }
 
+int main() {
+    int sudoku[N][N] = {
+        {5, 3, 0, 0, 7, 0, 0, 0, 0},
+        {6, 0, 0, 1, 9, 5, 0, 0, 0},
+        {0, 9, 8, 0, 0, 0, 0, 6, 0},
+        {8, 0, 0, 0, 6, 0, 0, 0, 3},
+        {4, 0, 0, 8, 0, 3, 0, 0, 1},
+        {7, 0, 0, 0, 2, 0, 0, 0, 6},
+        {0, 6, 0, 0, 0, 0, 2, 8, 0},
+        {0, 0, 0, 4, 1, 9, 0, 0, 5},
+        {0, 0, 0, 0, 8, 0, 0, 7, 9}};
+
+    printf("Sudoku original:\n");
+    printTabela(sudoku);
+
+    if (resolverSudoku(sudoku)) {
+        printf("\nSudoku resolvido:\n");
+        printTabela(sudoku);
+    }
+    else {
+        printf("O Sudoku não pôde ser resolvido.\n");
+    }
+    return 0;
+}
